18f696245dedc1c5c2867123f74136c1408c3926
Viktor Melker <viktormelker@gmail.com>
2016-09-28 01:30:06 +0200

    Do base64 encoding on raw bytes instead of string

--- set1/challange1/base64Encode.py
+++ set1/challange1/base64Encode.py
@@ -2,7 +2,8 @@ import base64
 
 def main():
     hexString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    b64String = base64Encode(hexString)
+    hex_data = hexString.decode("hex")
+    b64String = base64Encode(hex_data)
     print (b64String)
 
 def base64Encode( hexString ):
