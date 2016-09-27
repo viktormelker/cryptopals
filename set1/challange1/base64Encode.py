b07d0551d7837dc9e9ffa3944c42e8ab26f8a2fc
Viktor Melker <viktormelker@gmail.com>
2016-09-28 01:22:19 +0200

    Lade till nya filer för challange1

---
+++ set1/challange1/base64Encode.py
@@ -0,0 +1,12 @@
+import base64
+
+def main():
+    hexString = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    b64String = base64Encode(hexString)
+    print (b64String)
+
+def base64Encode( hexString ):
+   return base64.b64encode(hexString)
+
+if __name__ == "__main__":
+    main()
